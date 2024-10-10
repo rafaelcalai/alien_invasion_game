062f63f83b3e6576d6898c4316fc4d2bbbd50b71
Rafael Calai <rafael.calai@pt.bosch.com>
2024-10-10 15:06:49 +0100

    Game skeleton created

---
+++ alien_invasion.py
@@ -0,0 +1,41 @@
+import sys
+import pygame
+
+from ship import Ship
+from setting import Settings
+
+
+class AlienInvasion:
+    """Overall class to manage assets and behavior."""
+
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption("Allien Invasion")
+        self.ship = Ship(self)
+    
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
